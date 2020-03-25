a3b6a03d6db506b53c089dc143d373b038268050
brandonstiles1 <brandonstiles@gmail.com>
2020-03-25 12:26:17 -0400

    Posts rendered

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import ApolloProvider from './ApolloProvider';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  ApolloProvider,
+  document.getElementById( 'root' )
 );
 
 // If you want your app to work offline and load faster, you can change
